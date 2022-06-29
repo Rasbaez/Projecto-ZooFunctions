0352ef2b01baa4411d2bc3b6ef89870135013318
Roberto Baez <rbaezsp@gmail.com>
2022-06-29 18:27:36 -0300

    req1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,16 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
 
-function getSpeciesByIds(ids) {
+const species = [...data.species]
+
+function getSpeciesByIds(...ids) {
   // seu código aqui
+
+  const animal = ids.map((id) => species.find(specie => specie.id === id))
+    
+  return animal
 }
+
+
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
 
 module.exports = getSpeciesByIds;
