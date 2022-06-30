4aa164bf5c1c6df2ada50855d33c706bcd641c9c
Roberto Baez <rbaezsp@gmail.com>
2022-06-30 09:40:24 -0300

    req 1 2 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,10 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  const { employees } = data;
+  const result = employees.find((employeer) => employeeName === employeer.firstName
+                                              || employeeName === employeer.lastName);
+  return employeeName === undefined ? {} : result;
 }
 
 module.exports = getEmployeeByName;
