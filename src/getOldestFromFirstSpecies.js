bf4812955b3fb58eeeb8ad84f625f921a3f0667b
Roberto Baez <rbaezsp@gmail.com>
2022-07-03 20:56:50 -0300

    req 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,17 +1,22 @@
 const data = require('../data/zoo_data');
 
-// const { species, employees } = data;
+const { species, employees } = data;
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-  // const obj = {};
-  // const residentsName = employees.map(
-  //   ({ firstName, lastName }) => `${firstName} ${lastName}`
-  // );
+  const animalsId = employees.find((employee) => employee.id === id).responsibleFor;
+  const firstAnimal = species.find((specie) => specie.id === animalsId[0]);
+  const oldestAnimal = firstAnimal.residents.reduce((prev, curr) => {
+    if (prev.age > curr.age) {
+      return prev;
+    }
+    return curr;
+  });
+  const { name, sex, age } = oldestAnimal;
+  const result = [name, sex, age];
 
-  // return residentsName;
+  return result;
 }
 
-// console.log();
+// console.log(getOldestFromFirstSpecies("56d43ba3-a5a7-40f6-8dd7-cbb05082383f"));
 
 module.exports = getOldestFromFirstSpecies;
